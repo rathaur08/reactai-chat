2ed61d317bd3e4c3340ece2741723c780f03dfda
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-10-17 15:28:46 +0530

    Add Functionalty Dark or Light Mode

--- src/components/QuestionAnswer.jsx
+++ src/components/QuestionAnswer.jsx
@@ -8,7 +8,7 @@ const QuestionAnswer = ({ item, index }) => {
       <div key={index + Math.random()} className={item.type == "q" ? "flex justify-end" : ""}>
         {
           item.type == "q" ?
-            <li key={index + Math.random()} className='text-right p-1 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit ml-auto text-white'
+            <li key={index + Math.random()} className='text-right p-1 border-8 dark:bg-zinc-700 bg-red-100 dark:border-zinc-700 border-red-100 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit ml-auto dark:text-white text-zinc-800'
             ><Answers ans={item.text} index={index} totalResult={1} type={item.type} /></li>
             : item.text.map((ansItem, ansIndex) => (
               <li key={ansIndex + Math.random()} className='text-start'><Answers ans={ansItem} index={ansIndex} type={item.type} totalResult={item.length} /></li>
--- src/components/RecentSearch.jsx
+++ src/components/RecentSearch.jsx
@@ -10,8 +10,8 @@ const RecentSearch = ({ resentHistory, setResentHistory, setSelectedHistory }) =
 
   return (
     <>
-      <div className='col-span-1 bg-zinc-800 pt-3'>
-        <h1 className='text-white text-2xl'>Recent Search
+      <div className='col-span-1 dark:bg-zinc-800 bg-red-100 pt-3'>
+        <h1 className='dark:text-white text-zinc-800 text-2xl'>Recent Search
           <button onClick={clearHistory} className='cursor-pointer'>
             <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" /></svg>
           </button>
@@ -19,7 +19,7 @@ const RecentSearch = ({ resentHistory, setResentHistory, setSelectedHistory }) =
 
         <ul className='text-left overflow-auto'>
           {resentHistory && resentHistory.map((item, index) => (
-            <li key={index + Math.random()} className='p-3 pl-4 border-bs border-zinc-600 text-white hover:bg-zinc-700 cursor-pointer'
+            <li key={index + Math.random()} className='p-3 pl-4 border-bs dark:border-zinc-600 border-red-200 dark:text-white text-zinc-800 dark:hover:bg-zinc-700 hover:bg-red-200 cursor-pointer'
               onClick={() => setSelectedHistory(item)}>{item.length > 20 ? item.slice(0, 30) + "..." : item}</li>
             // onClick={() => setQuestiony(item)}>{item.length > 20 ? item.slice(0, 30) + "..." : item}</li>
           ))
