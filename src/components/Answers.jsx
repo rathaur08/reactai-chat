7450eaa1e24c103f6694f94fbfdf5aa8f34f03cd
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-10-17 16:25:41 +0530

    Update Highlight Code in Search Result

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { checkHeading, replaceHeadingStars } from '../Helper';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+// import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import ReactMarkdown from 'react-markdown';
 
 const Answers = ({ ans, index, totalResult, type }) => {
   console.log("data-3", totalResult, ans, index, type);
@@ -19,12 +22,35 @@ const Answers = ({ ans, index, totalResult, type }) => {
   //   return /^(\*)(\*)(.*)\*$/.test(str);
   // }
 
+  const renderer = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, '')}
+          // style={dark} // dark style not working properly
+          language={match[1]}
+          PreTag="div"
+        />
+      ) : (
+        <code {...props} className={className}>
+          {children}
+        </code>
+      );
+    }
+  };
+
   return (
     <>
       {
         index == 0 && totalResult > 1 ? <span className='text-xl block text-white'>{answer}</span> :
-          heading ? <span className='pt-2 text-lg block text-white'>{answer}</span>
-            : <span className={type == 'q' ? "pl-1" : "pl-5"}>{answer}</span>
+          heading ? <span className='pt-2 text-lg block dark:text-white text-zinc-800'>{answer}</span>
+            : <span className={type == 'q' ? "pl-1" : "pl-5"}>
+              <ReactMarkdown components={renderer}>
+                {answer}
+              </ReactMarkdown>
+            </span>
       }
     </>
   )
